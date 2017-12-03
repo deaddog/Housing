fb511403f13383ba77c0366ff5eb8c6b78f93831
deaddog <mikkelslsl@gmail.com>
2017-12-03 12:30:03 +0100

    Defined the basic interfaces for retrieving house information.

---
+++ Housing.Parsing/IEstateAgentHouseParser.cs
@@ -0,0 +1,22 @@
+﻿using Housing.Models;
+using HtmlAgilityPack;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Housing.Parsing
+{
+    public interface IEstateAgentHouseParser
+    {
+        string SupportedDomain { get; }
+
+        Task<AddressLocation> GetAddress(HtmlDocument doc);
+
+        Task<Area> GetHouseArea(HtmlDocument doc);
+        Task<Area> GetGroundArea(HtmlDocument doc);
+
+        Task<double> GetPrice(HtmlDocument doc);
+        Task<EnergyTypes> GetEnergyType(HtmlDocument doc);
+
+        Task<IEnumerable<HouseImage>> GetImages(HtmlDocument doc);
+    }
+}
---
+++ Housing.Parsing/IHouseParser.cs
@@ -0,0 +1,12 @@
+﻿using Housing.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Housing.Parsing
+{
+    public interface IHouseParser
+    {
+        bool CanGetHouse(Uri houseUri);
+        Task<House> GetHouse(Uri houseUri);
+    }
+}
