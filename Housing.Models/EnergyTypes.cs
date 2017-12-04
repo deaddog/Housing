7f14afdbca8d1def2c1f457867fdd3a9fc32fc17
deaddog <mikkelslsl@gmail.com>
2017-12-04 19:04:27 +0100

    Added 'Unknown' to energy types - just to be sure.

--- Housing.Models/EnergyTypes.cs
+++ Housing.Models/EnergyTypes.cs
@@ -3,6 +3,7 @@
     //https://ens.dk/ansvarsomraader/energimaerkning-af-bygninger/det-viser-energimaerket
     public enum EnergyTypes
     {
+        Unknown,
         A2020,
         A2015,
         A2010,
